b6cd753dc4b5a0f0ee58ce16683d34415c359038
mehdi.bichari <mehdi.bichari@outscale.com>
2020-11-11 07:24:18 +0100

    Correcting url in scrapping tests

--- scrapping/__init__.py
+++ scrapping/__init__.py
@@ -1,5 +1,6 @@
+"""Constants for scrapping"""
 import os
 
 BASE_URL = "http://www.giantbomb.com/api/"
-API_KEY = os.environ['GIANTBOMB']
-SEARCH_PATTERN = BASE_URL + 'search?api_key=' + API_KEY
+API_KEY = os.environ["GIANTBOMB"]
+SEARCH_PATTERN = BASE_URL + "search/?api_key=" + API_KEY
