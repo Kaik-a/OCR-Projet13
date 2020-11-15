0b183926d5d9efce5249f39baf5522232762af3b
mehdi.bichari <mehdi.bichari@outscale.com>
2020-11-15 16:45:20 +0100

    Docstrings library views

--- library/views.py
+++ library/views.py
@@ -1,26 +1,57 @@
 """Views for library"""
+from django.http import HttpResponse
 
 
-def borrowed():
-    """Display borrowed games"""
+def borrowed(request, user: str) -> HttpResponse:
+    """
+    Display borrowed games for a given user.
+
+    :param request: Django's request
+    :param user: gamelender user
+    :rtype: HttpResponse
+    """
     return None
 
 
-def game():
-    """Display game"""
+def game(request, game_id: int) -> HttpResponse:
+    """
+    Load game's page.
+
+    :param request: Django's request
+    :param game_id: Game's id
+    :rtype: HttpResponse
+    """
     return None
 
 
-def lends():
-    """Display lended games"""
+def lends(request, user: str) -> HttpResponse:
+    """
+    Didplay lended games for a given user.
+
+    :param request: Django's request
+    :param user: gamelender user
+    :rtype: HttpResponse
+    """
     return None
 
 
-def results():
-    """Display results"""
+def results(request, query: str) -> HttpResponse:
+    """
+    Display results for a given query.
+
+    :param request: Django's request
+    :param query: query to look for
+    :rtype: HttpResponse
+    """
     return None
 
 
-def wanted():
-    """Display wanted games"""
+def wanted(request, user: str) -> HttpResponse:
+    """
+    Display wanted games for a given user.
+
+    :param request: Django's request
+    :param user: gamelender user
+    :rtype: HttpResponse
+    """
     return None
