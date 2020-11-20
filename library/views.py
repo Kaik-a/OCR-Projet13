e2313efc50e6f2e4762d9c6ad33a599d5a0fd623
mehdi.bichari <mehdi.bichari@outscale.com>
2020-11-20 14:49:50 +0100

    Filled views and upgrade base.html

--- library/views.py
+++ library/views.py
@@ -1,5 +1,13 @@
 """Views for library"""
+from typing import List, Union
+
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from accounts.models import CustomUser
+from library.models import Game, LenderedGame, WantedGame
+from scrapping.send_requests import send_request
 
 
 def borrowed(request, user: str) -> HttpResponse:
@@ -10,7 +18,18 @@ def borrowed(request, user: str) -> HttpResponse:
     :param user: gamelender user
     :rtype: HttpResponse
     """
-    return None
+    borrower: CustomUser = CustomUser.objects.get(id=user)
+    try:
+        borrowed_games: Union[
+            List[LenderedGame], LenderedGame
+        ] = LenderedGame.objects.filter(borrower=borrower)
+    except:
+        # TODO: message telling no games borrowed yet
+        return render(request, "borrowed.html")
+
+    context = {"borrowed_games": borrowed_games}
+
+    return render(request, "borrowed.html", context)
 
 
 def game(request, game_id: int) -> HttpResponse:
@@ -21,7 +40,13 @@ def game(request, game_id: int) -> HttpResponse:
     :param game_id: Game's id
     :rtype: HttpResponse
     """
-    return None
+    try:
+        game_: Game = Game.objects.get(id=game_id)
+    except:
+        # TODO "this game isn't in our db"
+        return render(request, "game.html")
+
+    return render(request, "game.html", {"game": game_})
 
 
 def lends(request, user: str) -> HttpResponse:
@@ -32,7 +57,17 @@ def lends(request, user: str) -> HttpResponse:
     :param user: gamelender user
     :rtype: HttpResponse
     """
-    return None
+    lender: CustomUser = CustomUser.objects.get(id=user)
+
+    try:
+        lendered_games: Union[
+            List[LenderedGame], LenderedGame
+        ] = LenderedGame.objects.filter(lender=lender)
+    except:
+        # TODO: "no game lendered"
+        return render(request, "lendered.html")
+
+    return render(request, "lendered.html", {"lendered_games": lendered_games})
 
 
 def results(request, query: str) -> HttpResponse:
@@ -43,7 +78,12 @@ def results(request, query: str) -> HttpResponse:
     :param query: query to look for
     :rtype: HttpResponse
     """
-    return None
+    try:
+        game_list = send_request(query=query, resources="game")
+    except:
+        return redirect(reverse("home"))
+
+    return render(request, "library:results", {"games": game_list})
 
 
 def wanted(request, user: str) -> HttpResponse:
@@ -54,4 +94,13 @@ def wanted(request, user: str) -> HttpResponse:
     :param user: gamelender user
     :rtype: HttpResponse
     """
-    return None
+    user_ = CustomUser.objects.get(id=user)
+
+    try:
+        wanted_games: Union[List[WantedGame], WantedGame] = WantedGame.objects.filter(
+            user=user_
+        )
+    except:
+        return redirect(reverse("home"))
+
+    return render(request, "wanted.html", {"wanted_games": wanted_games})
